ebd0c3e05da3b80ba41aa96c114da714b7dfa9f1
Floyddan <CountElqyd@users.noreply.github.com>
2025-08-02 12:18:40 +0800

    Add files via upload

---
+++ day4/Configuring Azure Active Directory.ps1
@@ -0,0 +1,17 @@
+﻿Identity
+Authenticate
+Authorize 
+Audit
+
+# Azure AD Configuration
+
+## Post Install
+- powershell
+1. turn off firewall
+Set-NetFirewallProfile -name public,private,domain -Enabled False
+
+2. giveIPaddress: 
+ncpa.cpl
+
+3. ChangeName
+Rename-Computer entraid

00a54578f9cc777b38945133a1ddbcb053885250
Floyddan <CountElqyd@users.noreply.github.com>
2025-08-09 09:55:39 +0800

    Add files via upload

---
+++ Day3/csr1000- enable radius.ps1
@@ -0,0 +1,16 @@
+﻿conf t
+int gi 1
+no shut
+ip add 10.41.1.5 255.255.255.0 
+aaa new-model
+aaa authentication login default group RADGROUP local
+aaa authorization exec default group RADGROUP local
+radius server WINDOWSRADIUS
+ address ipv4 10.41.1.8 auth-port 1813
+ key keykeymo
+aaa group radius RADGROUP
+ server 10.41.1.8
+line vty 0 4
+ transport input all
+ login authentication default
+end
---
+++ Day3/protectyourSwitch.ps1
@@ -0,0 +1,47 @@
+﻿CIA 
+Protect your Switch
+
+
+# level1: 
+local AAA - authentication, authorization aaccounting
+
+cisco:
+```
+who
+```
+
+
+accounting
+config t
+    username admin privilege 15 secret pass
+    username cisco privilege 15 secret pass
+    username rob privilege 15 secret pass
+    line vty 0 14
+    transport input all
+    login local
+end
+
+
+# level 2: encryption
+use public/private key pair security for encrypted data trafic
+
+## part 1 - create keys
+conf t
+ ip domain-lookup
+ ip domain name rivansecplus.com
+!! generate a public/private key pair
+crypto key generate rsa
+
+# level3 
+2fa
+
+
+# level4
+passkey
+
+
+### VERIFY FILE INTEGRITY
+
+certutil -hashfile 'filename' md5
+
+e7908933449613edc97e1b11180429d1
